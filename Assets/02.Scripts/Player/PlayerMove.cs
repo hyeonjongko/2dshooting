39dc7b848bdaa24c648cfa3a76845d3b5f721bc0
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2025-11-03 18:28:52 +0900

    R키를 누르고 있으면 플레이어가 자동으로 원점으로 가게끔 (Translate를 사용하세요)

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 //플레이어 이동
 public class PlayerMove : MonoBehaviour
@@ -17,25 +18,30 @@ public class PlayerMove : MonoBehaviour
 
     [Header("이동범위")]
     public float MinX = -2;
-    public float MaxX =  2;
+    public float MaxX = 2;
     public float MinY = -5;
-    public float MaxY =  0;
+    public float MaxY = 0;
 
-    public float LeftEnd =   -2.9f;
-    public float RightEnd =  2.9f;
-    public float TopEnd =    0.5f;
+    public float LeftEnd = -2.9f;
+    public float RightEnd = 2.9f;
+    public float TopEnd = 0.5f;
     public float BottomEnd = -5.5f;
 
     [Header("속도")]
     public float MaxSpeed = 10;
     public float MinSpeed = 1;
 
-    public float Run = 1.2f;
+    public float Run = 5;
+
+    [Header("거리")]
+
+    public Vector2 origin = new Vector2(0, 0);
+    public Vector2 destination = new Vector2(0, 0);
 
     //게임 오브젝트가 게임을 시작할 때
     void Start()
     {
-        
+
     }
 
     // 게임 오브젝트가 게임을 시작 후 최대한 많이
@@ -52,6 +58,49 @@ public class PlayerMove : MonoBehaviour
 
         Debug.Log($"h : {h}, v: {v}");
 
+        // 실습 2. 스피드 조작
+
+        //if(Input.GetKey("q"))
+        //{
+        //    Speed += 1;
+        //}
+        //else if(Input.GetKey("e"))
+        //{
+        //    Speed -= 1;
+        //}
+
+        if (Input.GetKeyDown("q")) //GetKey : 누르고 있는 동안 계속 실행 / GetKeyDown : 한 번만 실행
+        {
+            Speed++;
+            //if(Speed > MaxSpeed)
+            //{
+            //    Speed = MaxSpeed;
+            //}
+        }
+        else if (Input.GetKeyDown("e"))
+        {
+            Speed--;
+            //if(Speed < MinSpeed)
+            //{
+            //    Speed = MinSpeed;
+            //}
+        }
+
+        //Speed = Mathf.Max(MinSpeed, Mathf.Min(MaxSpeed, Speed));
+        Speed = Mathf.Clamp(Speed, MinSpeed, MaxSpeed);
+
+        float RunSpeed = Speed;
+
+        //실습 4번 : Shift키를 누르는 중에 이동속도가 1.2배 빨라지게
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            RunSpeed = RunSpeed * Run;
+        }
+        //else if (Input.GetKeyUp(KeyCode.LeftShift))
+        //{
+        //    Speed = Speed / Run;
+        //}
+
         // 2. 입력으로부터 방향을 구한다.
         // 벡터 : 크기와 방향을 표현하는 물리 개념
         Vector2 direction = new Vector2(h, v);
@@ -63,21 +112,12 @@ public class PlayerMove : MonoBehaviour
         Debug.Log($"direction : {direction.x},{direction.y}");
 
 
+
         // 3. 그 방향으로 이동한다.
         Vector2 position = this.transform.position; // 현재 위치
 
 
-        //실습 4번 : Shift키를 누르는 중에 이동속도가 1.2배 빨라지게
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            Speed = Speed * Run;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            Speed = Speed / Run;
-        }
-
-        Vector2 distance = direction * Speed * Time.deltaTime;
+        Vector2 distance = direction * RunSpeed * Time.deltaTime;
 
 
         //Translate 버전
@@ -116,55 +156,34 @@ public class PlayerMove : MonoBehaviour
         //    newPosition.y = MaxY;
         //}
 
-        // 실습 2. 스피드 조작
-
-        //if(Input.GetKey("q"))
-        //{
-        //    Speed += 1;
-        //}
-        //else if(Input.GetKey("e"))
-        //{
-        //    Speed -= 1;
-        //}
-
-        if (Input.GetKeyDown("q")) //GetKey : 누르고 있는 동안 계속 실행 / GetKeyDown : 한 번만 실행
-        {
-            Speed++;
-            //if(Speed > MaxSpeed)
-            //{
-            //    Speed = MaxSpeed;
-            //}
-        }
-        else if (Input.GetKeyDown("e"))
-        {
-            Speed--;
-            //if(Speed < MinSpeed)
-            //{
-            //    Speed = MinSpeed;
-            //}
-        }
-
-        //Speed = Mathf.Max(MinSpeed, Mathf.Min(MaxSpeed, Speed));
-        Speed = Mathf.Clamp(Speed, MinSpeed, MaxSpeed);
-
         // 실습 3. 양쪽 끝으로 가면 반대쪽 끝에서 다시 나오게
         if (newPosition.x < LeftEnd)
         {
             newPosition.x = RightEnd;
         }
-        else if(newPosition.x > RightEnd)
+        else if (newPosition.x > RightEnd)
         {
             newPosition.x = LeftEnd;
         }
-        if(newPosition.y > TopEnd)
+        if (newPosition.y > TopEnd)
         {
             newPosition.y = BottomEnd;
         }
-        else if(newPosition.y < BottomEnd)
+        else if (newPosition.y < BottomEnd)
         {
             newPosition.y = TopEnd;
         }
 
-        transform.position = newPosition;                   // 새로운 위치로 갱신
+
+        transform.position = newPosition; // 새로운 위치로 갱신
+
+        destination.x = newPosition.x - origin.x;
+        destination.y = newPosition.y - origin.y;
+
+        //실습 5. R키를 누르고 있으면 플레이어가 자동으로 원점으로 가게끔(Translate를 사용하세요.)
+        if (Input.GetKey("r"))
+        {
+            transform.Translate(-destination *Speed * Time.deltaTime );
+        }                        
     }
 }
