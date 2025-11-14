dd40bbadcdee7dd45e1c490087ad234bb4702777
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2025-11-14 09:31:04 +0900

    chore 풀링 수정

--- Assets/02.Scripts/Enemy/EnemyFactory.cs
+++ Assets/02.Scripts/Enemy/EnemyFactory.cs
@@ -27,22 +27,22 @@ public class EnemyFactory : MonoBehaviour
     }
     private void PoolInit()
     {
-        // 1. 탄창에 총알을 담을 수 있는 크기 배열로 만들어준다.
+        
         _enemyObjectPool = new GameObject[PoolSize];
         _traceEnemyObjectPool = new GameObject[PoolSize];
 
-        // 2. 탄창에 크기만큼 반복해서 
+        
         for (int i = 0; i < PoolSize; ++i)
         {
-            // 3. 총알을 생성한다.
+            
             GameObject bulletObject = Instantiate(_enemyPrefab, transform);
             GameObject assiBulletObject = Instantiate(_traceEnemyPrefab, transform);
 
-            // 4. 생성한 총알을 탄창에 담는다
+            
             _enemyObjectPool[i] = bulletObject;
             _traceEnemyObjectPool[i] = assiBulletObject;
 
-            // 5. 비활성화 한다.
+            
             bulletObject.SetActive(false);
             assiBulletObject.SetActive(false);
         }
